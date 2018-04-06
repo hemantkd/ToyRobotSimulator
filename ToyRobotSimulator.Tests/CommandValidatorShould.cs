e3804996e6b681b4ae2f252b929d6b6ded167fa9
Hemant Kumar Dundigalla <d.hemant.kr@gmail.com>
2018-04-06 21:19:28 +0100

    Adding the necessary NuGet packages to support testing in the ToyRobotSimulator.Tests .netcore class library

---
+++ ToyRobotSimulator.Tests/CommandValidatorShould.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace ToyRobotSimulator.Tests
+{
+    [TestFixture]
+    public class CommandValidatorShould
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+        
+    }
+}
