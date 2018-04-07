efcbf4b8b1a82f46113903d6f6b5be65c16e5529
Hemant Kumar Dundigalla <d.hemant.kr@gmail.com>
2018-04-07 13:34:15 +0100

    Added Scenario: "MOVE should move the toy robot one unit forward in the direction it is currently facing"

---
+++ ToyRobotSimulator/ToyRobot.cs
@@ -0,0 +1,33 @@
+﻿namespace ToyRobotSimulator
+{
+    public class ToyRobot
+    {
+        public ToyRobot(int xCoordinate, int yCoordinate, Direction facing)
+        {
+            XCoordinate = xCoordinate;
+            YCoordinate = yCoordinate;
+            Facing = facing;
+        }
+
+        public Direction Facing { get; private set; }
+
+        public int YCoordinate { get; private set; }
+
+        public int XCoordinate { get; private set; }
+
+        public void Move()
+        {
+            switch (Facing)
+            {
+                case Direction.East: XCoordinate++;
+                    break;
+                case Direction.West: XCoordinate--;
+                    break;
+                case Direction.North: YCoordinate++;
+                    break;
+                case Direction.South: YCoordinate--;
+                    break;
+            }
+        }
+    }
+}
