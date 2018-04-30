31a693f7a0a3f71f01ea5d7c5995b2a6cba7d6f8
Hemant Kumar Dundigalla <d.hemant.kr@gmail.com>
2018-04-30 13:21:28 +0100

    Adding Autofac IoC for the ConsoleUI app

--- ToyRobotSimulator.ConsoleUI/Program.cs
+++ ToyRobotSimulator.ConsoleUI/Program.cs
@@ -1,24 +1,32 @@
-﻿using ToyRobotSimulator.AppServices;
+﻿using Autofac;
+using ToyRobotSimulator.AppInterfaces;
+using ToyRobotSimulator.AppServices;
+using ToyRobotSimulator.CommonAppInterfaces;
 using ToyRobotSimulator.CommonAppServices;
 
 namespace ToyRobotSimulator.ConsoleUI
 {
     class Program
     {
+        private static IContainer Container { get; set; }
+
         static void Main()
         {
-            // TODO: Use an IoC Framework to resolve the dependencies
+            var builder = new ContainerBuilder();
 
-            var commandTextValidator = new CommandTextValidator();
-            var userInteractionService = new UserInteractionService(commandTextValidator);
-            
-            var commandControl = new CommandControl(
-                commandExecutor: new CommandExecutor(userInteractionService),
-                userInteractionService: userInteractionService,
-                toyRobot: new ToyRobot()
-            );
-            
-            commandControl.Start();
+            builder.RegisterType<CommandExecutor>().As<ICommandExecutor>();
+            builder.RegisterType<CommandTextValidator>().As<ICommandTextValidator>();
+            builder.RegisterType<UserInteractionService>().As<IUserInteractionService>().InstancePerLifetimeScope(); // Instance of UserIteractionService is shared within a single ILifetimeScope
+            builder.RegisterType<CommandControl>().AsSelf();
+            builder.RegisterType<ToyRobot>().AsSelf();
+
+            Container = builder.Build();
+
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var commanControl = scope.Resolve<CommandControl>();
+                commanControl.Start();
+            }
         }
     }
 }
