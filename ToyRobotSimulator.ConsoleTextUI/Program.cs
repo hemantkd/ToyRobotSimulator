63e85eb85096d9fe7fd3617763fa56544e533ba5
Hemant Kumar Dundigalla <d.hemant.kr@gmail.com>
2018-04-30 13:00:03 +0100

    Adding Autofac to register and resolving components

--- ToyRobotSimulator.ConsoleTextUI/Program.cs
+++ ToyRobotSimulator.ConsoleTextUI/Program.cs
@@ -1,23 +1,32 @@
-﻿using ToyRobotSimulator.CommonAppServices;
+﻿using Autofac;
+using ToyRobotSimulator.CommonAppInterfaces;
+using ToyRobotSimulator.CommonAppServices;
+using ToyRobotSimulator.TextAppInterfaces;
 using ToyRobotSimulator.TextAppServices;
 
 namespace ToyRobotSimulator.ConsoleTextUI
 {
     class Program
     {
+        private static IContainer Container { get; set; }
+
         static void Main(string[] args)
         {
-            // TODO: Use an IoC Framework to resolve the dependencies
-            var textCommandControl = new TextCommandControl(
-                commandTextExecutor: new CommandTextExecutor(
-                    commandTextValidator: new CommandTextValidator(),
-                    userInteractionByTextService: new UserInteractionByTextService()
-                ),
-                userInteractionByTextService: new UserInteractionByTextService(),
-                toyRobot: new ToyRobot()
-            );
+            var builder = new ContainerBuilder();
 
-            textCommandControl.Start();
+            builder.RegisterType<CommandTextExecutor>().As<ICommandTextExecutor>();
+            builder.RegisterType<CommandTextValidator>().As<ICommandTextValidator>();
+            builder.RegisterType<UserInteractionByTextService>().As<IUserInteractionByTextService>();
+            builder.RegisterType<TextCommandControl>().AsSelf();
+            builder.RegisterType<ToyRobot>().AsSelf();
+
+            Container = builder.Build();
+
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var textCommanControl = scope.Resolve<TextCommandControl>();
+                textCommanControl.Start();
+            }
         }
     }
 }
